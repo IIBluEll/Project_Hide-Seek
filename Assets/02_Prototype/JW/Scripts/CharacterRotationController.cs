334fdf8f32de34c7f0f0351d65dd26545b98fbc0
LeeHyunMin <hmlee4135@naver.com>
2026-08-09 17:51:10 +0900

    캐릭터 컨트롤러 회전 보간 추가

--- Assets/02_Prototype/JW/Scripts/CharacterRotationController.cs
+++ Assets/02_Prototype/JW/Scripts/CharacterRotationController.cs
@@ -1,27 +1,48 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CharacterRotationController : MonoBehaviour
 {
     private Transform _playerBody;
     [SerializeField] private float _mouseSensitive;
+    [SerializeField, Min(0f)] private float _rotationSmoothSpeed = 30f;
 
     private float _yaw;
+    private float _targetYaw;
+
+    private void Update()
+    {
+        if (_playerBody == null)
+            return;
+
+        float interpolation = CalculateInterpolation(Time.unscaledDeltaTime);
+        _yaw = Mathf.LerpAngle(_yaw, _targetYaw, interpolation);
+        _playerBody.localRotation = Quaternion.Euler(0f, _yaw, 0f);
+    }
 
     public void Init(Transform body)
     {
         _playerBody = body;
         _yaw = _playerBody.localEulerAngles.y;
+        _targetYaw = _yaw;
     }
 
     public void Rotate(Vector2 value)
     {
-        _yaw += value.x * _mouseSensitive;
-        _playerBody.localRotation = Quaternion.Euler(0f, _yaw, 0f);
+        _targetYaw += value.x * _mouseSensitive;
     }
+
     public void SetYRotation(Vector3 rotation)
     {
         _yaw = rotation.y;
+        _targetYaw = _yaw;
         _playerBody.localRotation = Quaternion.Euler(0f, _yaw, 0f);
+    }
+
+    private float CalculateInterpolation(float deltaTime)
+    {
+        if (_rotationSmoothSpeed <= 0f)
+            return 1f;
+
+        return 1f - Mathf.Exp(-_rotationSmoothSpeed * deltaTime);
     }
 }
