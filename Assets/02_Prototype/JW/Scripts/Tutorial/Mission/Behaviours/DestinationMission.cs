8262417a3a72fc954bf1ee000ce952a05a412bf5
jinwoo715 <91857768+jinwoo715@users.noreply.github.com>
2026-08-09 22:00:38 +0900

    .

--- Assets/02_Prototype/JW/Scripts/Tutorial/Mission/Behaviours/DestinationMission.cs
+++ Assets/02_Prototype/JW/Scripts/Tutorial/Mission/Behaviours/DestinationMission.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class DestinationMission : MissionBase
 {
+    [SerializeField] private GameObject _barrior;
+
+    private void Awake()
+    {
+        _barrior.SetActive(true);
+    }
+
+    protected override void OnBeginMission()
+    {
+        _barrior.SetActive(false);
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (IsRunning && other.CompareTag("Player"))
